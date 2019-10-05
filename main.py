95e81cbd15d0e04eda59ab7ac5395be6d6d7ff73
Adam <adamhoflin98@gmail.com>
2019-10-05 18:54:36 +0200

    email implemented

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ from bs4 import BeautifulSoup
 import requests
 import json
 from urllib.parse import urlparse, parse_qs
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import os
+from dotenv import load_dotenv
 
+# loading environment variables
+load_dotenv()
 
 # Running the scraper
 site = requests.post('http://www.exjobbmdh.se/jobb_lista.asp', data={'omradesid': 2})
@@ -21,8 +28,47 @@ for index, tr in enumerate(t_body):
         job_id = parse_qs(url.query)['jobbid']
         jobs[job_id[0]] = {'title': title, 'link': link}
 
-# Writing list to json-file
+
+# Writing list to temporary json-file
+with open('data/temp_jobs.json', 'w', encoding='utf-8') as outfile:
+    outfile.write(json.dumps(jobs, indent=4, sort_keys=True, ensure_ascii=False))
+
+
+old_data = json.load(open("data/jobs.json"))
+new_data = json.load(open("data/temp_jobs.json"))
+
+if old_data == new_data:
+    print("No changes made...")
+    exit(0)
+
 with open('data/jobs.json', 'w', encoding='utf-8') as outfile:
     outfile.write(json.dumps(jobs, indent=4, sort_keys=True, ensure_ascii=False))
+
+# Send email
+my_email = os.getenv("email")
+my_password = os.getenv("password")
+receiver_email = my_email  # EHM... Fix this maybe?
+
+msg = MIMEMultipart('alternative')
+msg['Subject'] = 'Test mail'
+msg['From'] = my_email
+msg['To'] = receiver_email
+
+html = '<html><body><p>Hi,<br>http://www.exjobbmdh.se has been updated. Here are the current jobs available:</p><br>'
+
+for job in jobs:
+    html += "<a href=\"http://www.exjobbmdh.se/" + jobs[job]['link'] + "\">" + jobs[job]['title'] + "</a><br><br>"
+
+html += '</body></html>'
+
+message = MIMEText(html, 'html')
+msg.attach(message)
+
+s = smtplib.SMTP_SSL('smtp.gmail.com')
+s.login(my_email, my_password)
+s.sendmail(my_email, receiver_email, msg.as_string())
+s.quit()
+
+
 
 
